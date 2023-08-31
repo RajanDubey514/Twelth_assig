e80aeafe66f134604ac30183f3b541632389edc4
Rajan Dubey <rajandubey514@gmail.com>
2023-08-31 17:16:13 +0530

    card ready to degine

---
+++ my-app/src/component/Task1/Data.js
@@ -0,0 +1,34 @@
+// Array of objects for pricing
+var objArr = [
+    {
+        plan_rs: '$10/mth',
+        plan: 'Basic plan',
+        billed: 'Billed annually',
+        feature_list_1 : 'Access to all basic feratures',
+        feature_list_2 : 'Basic reporting and analytics',
+        feature_list_3 : 'Up to 10 individual users',
+        feature_list_4 : '20GB individual data each user',
+        feature_list_5 : 'Basic chat and email support',
+    },
+    {
+        plan_rs: '$20/mth',
+        plan: 'Business plan',
+        billed: 'Billed annually',
+        feature_list_1 : '200+ integrations',
+        feature_list_2 : 'Advanced reporting and analytics',
+        feature_list_3 : 'Up to 20 individual users',
+        feature_list_4 : '40GB individual data each user',
+        feature_list_5 : 'Priority chat and email support',
+    },
+    {
+        plan_rs: '$40/mth',
+        plan: 'Enterprise plan',
+        billed: 'Billed annually',
+        feature_list_1 : 'Advanced custom fields',
+        feature_list_2 : 'Audit log and data history',
+        feature_list_3 : 'Unlimited individual users',
+        feature_list_4 : 'Unlimited individual data',
+        feature_list_5 : 'Personalised + priotity service',
+    },
+];
+export default objArr;
--- my-app/src/component/Task1/Pricing.js
+++ my-app/src/component/Task1/Pricing.js
@@ -1,21 +1,56 @@
 import React from 'react'
-import { Grid,Typography } from '@mui/material'
+import { Grid, Typography, Card, CardActions, CardContent, Button } from '@mui/material'
+import  objArr  from './Data'
 import './pricing.css';
+import DoneIcon from '@mui/icons-material/Done';
 
 function Pricing() {
-  return (
-   <Grid container className="pricing_comntainer">
-    <Grid item lg={12} md={12} sm={12} xs={12} className="pricing_head_container">
-    <Typography variant="p" className="pricing_head">Pricing</Typography>
-    </Grid> 
-    <Grid item lg={12} md={12} sm={12} xs={12} className="pricing_header_container">
-    <Typography variant="p" className="pricing_header">Simple, transparent pricing</Typography>
-    </Grid>
-    <Grid item lg={12} md={12} sm={12} xs={12} className="pricing_para_container">
-    <Typography variant="p" className="pricing_para">We believe Untitled should be accessible to all companies, no matter the size.</Typography>
-    </Grid>
-   </Grid>
-  )
+    return (
+        <Grid container className="pricing_comntainer">
+            <Grid item lg={12} md={12} sm={12} xs={12} className="pricing_head_container">
+                <Typography variant="p" className="pricing_head">Pricing</Typography>
+            </Grid>
+            <Grid item lg={12} md={12} sm={12} xs={12} className="pricing_header_container">
+                <Typography variant="p" className="pricing_header">Simple, transparent pricing</Typography>
+            </Grid>
+            <Grid item lg={12} md={12} sm={12} xs={12} className="pricing_para_container">
+                <Typography variant="p" className="pricing_para">We believe Untitled should be accessible to all companies, no matter the size.</Typography>
+            </Grid>
+            <Grid container>
+            { objArr.map((data , key) => {
+                    return (
+                        <Grid item lg={12} md={12} sm={12} xs={12} key={key}>
+                            <Card sx={{ maxWidth: 345 }}>
+                                <CardContent>
+                                    <Grid>{data.plan_rs}</Grid>
+                                    <Grid>{data.plan}</Grid>
+                                    <Grid>{data.billed}</Grid>
+                                    <Grid>
+                                    <DoneIcon />
+                                    {data.feature_list_1}</Grid>
+                                    <Grid>
+                                    <DoneIcon />
+                                    {data.feature_list_2}</Grid>
+                                    <Grid>
+                                    <DoneIcon />
+                                    {data.feature_list_3}</Grid>
+                                    <Grid>
+                                    <DoneIcon />
+                                    {data.feature_list_4}</Grid>
+                                    <Grid>
+                                    <DoneIcon />
+                                    {data.feature_list_5}</Grid>                                    
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">Get started</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>)
+                })
+            }
+            </Grid>
+        </Grid>
+    )
 }
 
 export default Pricing
